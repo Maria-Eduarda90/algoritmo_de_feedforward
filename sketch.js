3931d8e2689171058c8731b1959cd4496bdef02e
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2023-11-07 21:29:52 -0300

    instantiating my Matrix class

--- sketch.js
+++ sketch.js
@@ -2,9 +2,10 @@ function setup() {
   createCanvas(500, 500);
   background(0);
 
-  var matrix = new Matrix(3, 3);
+  var matrix = new Matrix(2, 2);
+  var matrix2 = new Matrix(2, 2);
+  Matrix.add(matrix, matrix2);
   
-  console.log(matrix);
 }
 
 function draw() {
