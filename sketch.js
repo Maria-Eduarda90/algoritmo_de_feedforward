1b89aef87d79b3b456e9266210e7faa2a5888eef
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2023-11-18 00:24:22 -0300

    function to multiply matrices

--- sketch.js
+++ sketch.js
@@ -2,8 +2,10 @@ function setup() {
   createCanvas(500, 500);
   background(0);
 
-  var matrix = new Matrix(2, 2);
-  var matrix2 = new Matrix(2, 2);
+  var matrix = new Matrix(1, 2);
+  var matrix2 = new Matrix(2, 1);
+  console.log(matrix.data, matrix2.data);
+  console.log(Matrix.multiply(matrix, matrix2).data);
   Matrix.add(matrix, matrix2);
   
 }
